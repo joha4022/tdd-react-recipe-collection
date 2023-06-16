9f544f0356868c5d126885337d137615450732e7
juju <joha4022@gmail.com>
2023-06-16 14:32:34 -0700

    recipe-collection is able to collect recipe on submit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [recipeFormShown, showRecipeForm] = useState(false);
+  const [recipeName, setRecipeName] = useState(false);
+  const [instructions, setInstructions] = useState(false);
+  const [recipe, setRecipe] = useState([]);
+
+
+  let submitRecipe = (rec, instructions) => {
+    let currentRecipes = recipe;
+    let recipeCompiler = {
+      recipe: '',
+      instructions: ''
+    };
+    recipeCompiler.recipe = rec;
+    recipeCompiler.instructions = instructions;
+    console.log(recipeCompiler)
+    currentRecipes.push(recipeCompiler);
+    setRecipe([...recipe], recipeCompiler);
+  };
+
+  const DisplayRecipes = ({recipe}) => {
+      return(
+        <>
+          {recipe.map((e, i) =>
+          (
+            <div key={i}>
+              <div>Recipe Name: {e.recipe}</div>
+              <div>Instructions: {e.instructions}</div>
+            </div>
+          ))}
+        </>
+      )
+    }
+  
   return (
     <div className='App'>
       <h1>My Recipes</h1>
-      <p data-testid='recipe-list'>There are no recipes to list</p>
+      {
+        recipeFormShown ? 
+          <>
+            <form id="recipe-form" name='recipe-form' onSubmit={submitRecipe}>
+              <label htmlFor="newRecipeName">Recipe name: </label>
+              <input type="text" id="newRecipeName" onChange={(e) => {setRecipeName(e.target.value)}}/>
+              <label htmlFor="newRecipeInstructions">Instructions:</label>
+              <textarea id="newRecipeInstructions" placeholder="write recipe instructions here..." onChange={(e) => {setInstructions(e.target.value)}}/>
+              <input type="submit" onClick={(e) => {e.preventDefault(); submitRecipe(recipeName, instructions); document.querySelector('#current-status').textContent = 'My Recipes'}}/>
+            </form>
+          </>
+          : 
+            <button onClick={ () => showRecipeForm(!recipeFormShown) }>Add Recipe</button>
+      }
+      <p data-testid='recipe-list' id='current-status'>There are no recipes to list</p>
+      <DisplayRecipes recipe={recipe}/>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event';
 
 describe('recipe collection', () => {
   let recipeHeader;
-  let recipeList;
+  let button;
 
   beforeEach(() => {
     render(<App />);
     recipeHeader = screen.getByText(/my recipes/i);
-    recipeList = screen.getByText(/there are no recipes to list/i);
+    button = screen.getByRole('button', {name: 'Add Recipe'});
   })
 
   it('should render App and read "My Recipes"', () => {
@@ -16,10 +17,46 @@ describe('recipe collection', () => {
   });
 
   it('should display "There are no recipes to list" beneath the heading', () => {
+    let recipeList = screen.getByText(/there are no recipes to list/i);
     expect(recipeList).toBeInTheDocument();
+    expect(recipeHeader.compareDocumentPosition(recipeList)).toBe(4);
   })
 
-  it('should have recipe-list underneath header', () => {
-    expect(recipeHeader.compareDocumentPosition(recipeList)).toBe(4);
+  it('should contain an "Add Recipe" button underneath header', () => {
+    expect(button).toBeInTheDocument();
+    expect(recipeHeader.compareDocumentPosition(button)).toBe(4);
+  })
+
+  it('should open a form when "Add Recipe" button is clicked', async () => {
+    userEvent.click(button);
+
+    let form = await screen.findByRole('form', undefined, {timeout:3000});
+
+    expect(form).toBeInTheDocument();
+
+    expect(screen.getByRole('textbox', {name: /Recipe name/i})).toBeInTheDocument();
+    expect(screen.getByRole('textbox', {name: /instructions/i})).toBeInTheDocument();
+
+    button = screen.queryByRole('button', {name: 'Add Recipe'});
+    expect(button).toBeNull();
+  })
+
+  it('should list submitted recipes under "My Recipes"', async () => {
+    userEvent.click(button);
+
+    let form = await screen.findByRole('form', undefined, {timeout:3000});
+    expect(form).toBeInTheDocument();
+    expect(screen.getByRole('textbox', {name: /Recipe name/i})).toBeInTheDocument();
+    expect(screen.getByRole('textbox', {name: /instructions/i})).toBeInTheDocument();
+
+    let recipeNameBox = screen.getByRole('textbox', {name: /Recipe name/i});
+
+    userEvent.type(recipeNameBox, 'Tofu Scramble Tacos');
+
+    let submit = screen.getByRole('button', {name: 'Submit'});
+    userEvent.click(submit);
+
+    let recipe = await screen.findByText(/taco/i);
+    expect(recipe).toBeInTheDocument();
   })
 })
